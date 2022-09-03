c70629067801bbbd9046e505cfbba372710c5a2a
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2022-09-02 22:04:20 -0600

    Updated calendar.

--- src/calendar/calendar.tsx
+++ src/calendar/calendar.tsx
@@ -9,30 +9,33 @@ export interface CalendarProps {
   className?: string;
 }
 
+const HEIGHT = 60;
+
 export function Calendar({ domain, className, style }: CalendarProps) {
   const height = domain.dateHeight;
 
   return (
-    <Scroll
-      domain={domain.scrollDomain}
-      className={className}
-      style={style}
-    >
-      {(scrollDomain) => {
-        const startIndex = Math.floor(scrollDomain.top / height) - 1;
-        const endIndex = Math.ceil(scrollDomain.bottom / height) + 1;
+    <Scroll domain={domain.scrollDomain} className={className} style={style}>
+      {(scrollableDomain) => {
+        const startIndex =
+          scrollableDomain.top <= 0
+            ? Math.ceil(scrollableDomain.top / HEIGHT)
+            : Math.floor(scrollableDomain.top / HEIGHT);
+        const endIndex = Math.ceil(scrollableDomain.bottom / HEIGHT);
+        const amount = endIndex - startIndex;
         const children: React.ReactNode[] = [];
 
-        for (let x = startIndex; x < endIndex; x++) {
+        for (let i = -1; i <= amount; i++) {
+          const position = i * HEIGHT - (scrollableDomain.top % HEIGHT);
           const date = new Date(domain.today);
-          date.setDate(date.getDate() + x);
+          date.setDate(date.getDate() + i + startIndex);
 
           const style: React.CSSProperties = {
             position: "absolute",
-            transform: `translate(${0}px, ${x * height}px)`,
+            transform: `translate(${0}px, ${position}px)`,
           };
 
-          children.push(<SideDate date={date} key={x} style={style} />);
+          children.push(<SideDate date={date} key={i} style={style} />);
         }
 
         return children;
